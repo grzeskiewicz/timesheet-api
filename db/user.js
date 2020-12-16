8e6e14110c564d12cfe161261d19ce23848d0eb1
grzeskiewicz <benuch91@gmail.com>
2020-12-16 10:11:44 +0100

    Heroku mysql

--- db/user.js
+++ db/user.js
@@ -5,7 +5,7 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const date = require('../date');
-const dbConfig=require('dbConfig');
+const dbConfig = require('./dbConfig');
 
 
 const bodyParser = require('body-parser');
@@ -23,8 +23,30 @@ const connection = mysql.createConnection({
     port: 3306
 }); */
 
-const connection = mysql.createConnection(dbConfig.config);
+let connection;
 
+function handleDisconnect() {
+    connection = mysql.createConnection(dbConfig.config); // Recreate the connection, since
+    // the old one cannot be reused.
+
+    connection.connect(function (err) {              // The server is either down
+        if (err) {                                     // or restarting (takes a while sometimes).
+            console.log('error when connecting to db:', err);
+            setTimeout(handleDisconnect, 2000); // We introduce a delay before attempting to reconnect,
+        }                                     // to avoid a hot loop, and to allow our node script to
+    });                                     // process asynchronous requests in the meantime.
+    // If you're also serving http, display a 503 error.
+    connection.on('error', function (err) {
+        console.log('db error', err);
+        if (err.code === 'PROTOCOL_CONNECTION_LOST') { // Connection to the MySQL server is usually
+            handleDisconnect();                         // lost due to either server restart, or a
+        } else {                                      // connnection idle timeout (the wait_timeout
+            throw err;                                  // server variable configures this)
+        }
+    });
+}
+
+handleDisconnect();
 
 const comparePassword = function (password, hash) {
     return bcrypt.compareSync(password, hash);
