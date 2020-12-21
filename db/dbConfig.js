4d3bf328178e7c1160100570362720301c5474ec
grzeskiewicz <benuch91@gmail.com>
2020-12-21 15:01:25 +0100

    AWS

--- db/dbConfig.js
+++ db/dbConfig.js
@@ -1,16 +1,16 @@
 
-/*
+
 module.exports = {
     config: {
-        host: 'eu-cdbr-west-03.cleardb.net',
-        user: 'ba79b279a6d840',
-        password: 'a6c1b348',
-        database: 'heroku_6077f9353d7377d',
+        host: 'timesheetdb.c3d81nauj02k.eu-central-1.rds.amazonaws.com',
+        user: 'admin',
+        password: 'c9q7wd1q4KJ04pq5gtWf',
+        database: 'timesheet',
         port: 3306
     }
-} */
+} 
 
-
+/*
 module.exports = {
     config: {
         host: 'localhost',
@@ -20,5 +20,5 @@ module.exports = {
         port: 3306
     }
 }
-
+*/
 
